1a444b4c81e75dbc519bc41a5d7f3acd2f254ece
Yunus O. KAREEM <solayunus19@gmail.com>
2021-03-17 15:31:53 -0700

    update profile.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './pages/App';
 
 import "./assets/css/nucleo-icons.css";
 import "./assets/scss/blk-design-system-react.scss?v=1.2.0";
 import "./assets/demo/demo.css";
 
 import ProfilePage from "./pages/ProfilePage.js";
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+    <Route
+        path="/"
+        render={(props) => <ProfilePage {...props} />}
+      />
+
+      </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
